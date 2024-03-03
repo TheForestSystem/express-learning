0c7a0bbcba721442a6eb79f4951c72b0c07e3522
PrincessFoxx <mfcpij@gmail.com>
2024-03-03 13:39:56 -0600

    Change logging format

--- src/middleware/LoggingMiddleware.ts
+++ src/middleware/LoggingMiddleware.ts
@@ -5,10 +5,12 @@ import express, { Request, Response, NextFunction } from 'express';
  */
 export default class LoggingMiddleware {
     static logRequest(req: Request, res: Response, next: NextFunction) {
+        console.log(`--- Logging Request ---`)
         console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
         console.log(`[${new Date().toISOString()}] Request Body: ${JSON.stringify(req.body)}`);
         res.on('finish', () => {
             console.log(`[${new Date().toISOString()}] Response Status Code: ${res.statusCode}`);
+            console.log(`--- End Logging Request ---`)
         });
         next();
     }
